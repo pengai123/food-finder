45402719e88a2ff08322dc991c3c526184518c04
Peng Ai <aaaipeng@gmail.com>
2021-03-26 22:27:06 -0700

    added reponsive navbar and menu

--- client/src/components/ItemDetail.jsx
+++ client/src/components/ItemDetail.jsx
@@ -3,14 +3,9 @@ import axios from "axios";
 
 export default function ItemDetail({ match, location }) {
 
-	const [itemId, setItemId] = useState("");
-	const [itemName, setItemName] = useState("");
 	const [icon, setIcon] = useState("")
 
 	useEffect(() => {
-		console.log('id:', match.params.id)
-		setItemId(match.params.id)
-		setItemName(match.params.name)
 
 		//handle direct access to links
 		if (location.currentBanner) {
@@ -18,20 +13,13 @@ export default function ItemDetail({ match, location }) {
 			setIcon(location.currentBanner.images.icon)
 		}
 
-		return () => {
-			setItemId("")
-			setItemName("")
-			if (location.currentBanner) {
-				setIcon("")
-			}
-		}
 	}, [])
 
 	return (
 		<div className="page-container item-detail-container">
 			<h1>Item Detail</h1>
-			<h2>item id: {itemId}</h2>
-			<h2>item name: {itemName}</h2>
+			<h2>item id: {match.params.id}</h2>
+			<h2>item name: {match.params.name}</h2>
 			{location.currentBanner && <div className="icon-container">
 				<img src={icon} className="icon"></img>
 			</div>}
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Link } from "react-router-dom"
 
 export default function Nav() {
+
+	const navLinksRef = useRef(null)
+	const menuIconRef = useRef(null)
+
+	const toggleMenu = e => {
+		document.getElementById("menu-icon").classList.toggle("menu-active")
+		
+		// different method with useRef
+		// menuIconRef.current.classList.toggle("menu-active")
+	}
+
+	const handleClickOutside = e => {
+		if (navLinksRef &&
+			!navLinksRef.current.contains(e.target) &&
+			!menuIconRef.current.contains(e.target) &&
+			document.getElementById("menu-icon").classList.contains("menu-active")) {
+			document.getElementById("menu-icon").classList.remove("menu-active")
+		}
+	}
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClickOutside)
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		}
+	}, [])
+
 	return (
 		<div className="navbar">
 			<Link to="/" className="navbar-logo link">
 				LOGO
 			</Link>
-			<Link to="/about" className="navbar-item link">
-				About
-			</Link>
-			<Link to="/shop" className="navbar-item link">
-				Shop
-			</Link>
+			<div id="menu-icon" className="menu-icon" onClick={toggleMenu} ref={menuIconRef}>
+				<div className="line-1 menu-icon-line"></div>
+				<div className="line-2 menu-icon-line"></div>
+				<div className="line-3 menu-icon-line"></div>
+			</div>
+			<ul className="navbar-links" ref={navLinksRef}>
+				<li>
+					<Link to="/about" className="navbar-link link">About</Link>
+				</li>
+				<li>
+					<Link to="/shop" className="navbar-link link">Shop</Link>
+				</li>
+			</ul>
 		</div >
 	)
 }
